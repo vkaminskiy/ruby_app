8279f30e20f32564eee3e96722ae8267adba58f7
point <point@point-note.(none)>
2013-03-30 23:47:31 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '41c8213c944a06d0a1c0f024aeac696faa81eccf41e11c8a3521b3b166a1f19d87d18f636bafc797ec1fb66c7fe274dcfaf18e0376cdc0b6c70a63b85e589a8e'
